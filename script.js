fcd084b6b4f062223087fe25d7bc9a288217ae05
Kamil Stróżyk <strozykkamil@gmail.com>
2020-03-31 23:31:31 +0200

    add addItem()

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function addItem() {
-    console.log("Hello World!")
+    let table = document.getElementById('table').getElementsByTagName('tbody')[0];
+    let row = table.insertRow();
+    let itemsCount = document.getElementById('itemsContainer').rows.length - 1;
+
+    let authorCell = row.insertCell(0)
+    let titleCell = row.insertCell(1);
+    let buttonsCell = row.insertCell(2);
+
+    authorCell.appendChild(createElement('input', 'text', null, null));
+    titleCell.appendChild(createElement('input', 'text', null, null));
+    buttonsCell.appendChild(createElement('button', 'button', 'Save', 'saveItem(' + itemsCount + ')'));
+    buttonsCell.appendChild(createElement('button', 'button', 'Remove', 'deleteItem(' + itemsCount + ')'));
+}
+
+function createElement(elementType, elementConcreteType, innerHTML, attribute) {
+    let element = document.createElement(elementType);
+    element.type = elementConcreteType;
+    if (innerHTML != null && innerHTML != undefined) element.innerHTML = innerHTML;
+    if (attribute != null && attribute != undefined) element.setAttribute('onclick', attribute);
+    return element;
 }
